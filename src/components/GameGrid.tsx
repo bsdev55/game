40d46e8e539d43504a15f442ca936fdd5bc1e601
bsdev55 <bsdev55@users.noreply.github.com>
2023-06-02 22:10:12 +0200

    Displaying critic score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,7 +8,7 @@ const GameGrid = () => {
   return (
     <>
         {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm: 1,md: 2, lg: 3, xl: 4 }} padding='10px' spacing={10}>
+        <SimpleGrid columns={{sm: 1,md: 2, lg: 3 }} padding='10px' spacing={10}>
             {data.map((game) => (<GameCard key={game.id} game={game} />))}
         </SimpleGrid>
     </>
