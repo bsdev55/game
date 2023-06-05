378a7f8ea8e81d4fb1092bab2bccc51621ca79ca
bsdev55 <bsdev55@users.noreply.github.com>
2023-06-05 20:26:24 +0200

    filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -31,8 +31,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -5,7 +5,7 @@ import useData from "./useData";
 import { Genre } from "./useGenres";
 
 export interface Platform {
-    id: string;
+    id: number;
     name: string;
     slug: string;
 }
@@ -18,7 +18,9 @@ export interface Game {
     metacritic: number;
 }
 
-const useGames = (selectedGenre:Genre | null) => useData<Game>('/games', {params: { genres: selectedGenre?.id}}, [selectedGenre?.id]);
+const useGames = (selectedGenre:Genre | null, selectedPlatform: Platform | null) => 
+    useData<Game>('/games', {params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id}},
+    [selectedGenre?.id, selectedPlatform?.id]);
 
 
 export default useGames;
