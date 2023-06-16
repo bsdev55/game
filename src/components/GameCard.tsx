90521231ed9be93a187597705cb14dfd1d935a43
bsdev55 <bsdev55@users.noreply.github.com>
2023-06-16 20:04:41 +0200

    Add a dynamic page heading

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,11 +14,11 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow='hidden' textAlign='left'>
        <Image src={getCroppedImageUrl(game.background_image)} />
        <CardBody>
-        <Heading fontSize='1xl' marginBottom={2}>{game.name}<Emoji rating={game.rating_top}/> </Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
           <CriticScore score={game.metacritic}/>
         </HStack>
+        <Heading fontSize='1xl' marginBottom={2}>{game.name}<Emoji rating={game.rating_top}/> </Heading>
        </CardBody>
     </Card>
   )
