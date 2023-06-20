ff0abbcceb77ef7ba5f74573984f01606d3e1fe0
bsdev55 <bsdev55@users.noreply.github.com>
2023-06-20 20:30:20 +0200

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -5,31 +5,33 @@ import useData from "./useData";
 import { GameQuery } from "../App";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-    rating_top: number;
-    rating: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
+  rating: number;
 }
 
-const useGames = (gameQuery: GameQuery) => 
-    useData<Game>('/games',
+const useGames = (gameQuery: GameQuery) =>
+  useData<Game>(
+    "/games",
     {
-        params: { 
-            genres: gameQuery.genre?.id,
-            platforms: gameQuery.platform?.id, 
-            ordering: gameQuery.sortOrder,
-        },
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
+      },
     },
-    [gameQuery]);
-
+    [gameQuery]
+  );
 
 export default useGames;
